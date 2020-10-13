c904caee8dcef66994c1ee59d0191ee7ae9d9495
Jianjun <jianjx@amazon.com>
2020-10-12 20:01:34 -0400

    add ObjectRemove handling

--- README.md
+++ README.md
@@ -1,11 +1,55 @@
 
 # Welcome to FITS Data lake project. 
 
-This project is built with AWS CDK with Python
+## Introduction 
+Flexible Image Transport System (FITS) is the most commonly used file format in astronomy. Each FITS file contains one or more ASCII headers (metadata) and a binary data section as multi-dimensional array or tables. 
+Information about the data itself, such as instrument configuration, observation details, image origin, rize and coordinates, are stored in the headers as key value pairs. 
+
+Self-contained metadata format makes it possible for easy transport and sharing, it also provide challenges on the searchability and managibility of the data, especially when you are working on a large amount of data from different satallites or instruments. 
+
+Data lake is a centralized data repository, which has gain popularity in recently years as the size of the data grow exponentially in all industries. Unlike traditional data warehouse where high peformance relational database ( can be very expensive at large scale ) are used to store data in a central location and with pre-determined schema, data lake takes advantage of existing storage and centralize data schema in a data catalog. For example, to build a data lake on AWS, you can put structured or unstructed data in Amazon Simple Storage Service (Amazon S3) with different schema, automate an ETL crawler to aggregate the schemas into a fully managed data catalog. Then you can run sql querie against the data catalog and process your data. 
+
+Data lake has been used mostly in data analytics so far. In this solution, however, we will apply the concept and practice of building a serverless data lake in AWS cloud for FITS data. This approach can be applied to most of the scientific data, which containes metadata within or with external attachment of metadata files. 
+
 
 ## About CDK
+This project is built with AWS Cloud Development Kit (CDK) using Python. AWS CDK is an open source software development framework you can use to build and deploy your cloud infrastrures and application resources using your favorite programming language (as an alternative to using AWS CloudFormation). For more detailed information about AWS CDK, please visit https://aws.amazon.com/cdk/. 
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+Assume you have installed cdk and aws cli on the computer you are working on. 
+
+### CDK project basics
+The current fold structure is part of the scaffolding created using the following command. 
+```
+cdk init app --language python
+```
+Project code/test data are added on top 
+<pre>
+project_folder/
+|-- fits_data/                      # sample FITS files
+    |-- fits_samples/
+    |-- hubble_samples/
+    |-- turorials/
+|-- images/
+|-- my_fits_datalake/
+    |-- my_fits_datalake_stack.py   # the FitsDatalakeStack code
+!-- notebooks/                      # test jupyter notebook
+|-- python/                         # astropy package source
+|-- resources/
+    |-- fits_header_extractor       # lambda code for extracting FITS header info
+|-- resources_layer/
+    |-- astropy.zip                 # astropy package zip (zip of the python folder)
+|-- app.py                          # CDK app code
+|-- cdk.json                        # CDK config
+|-- .git/
+|-- .gitignore
+|-- test/
+|-- README.md
+|-- requirements.txt
+|-- setup.py
+|-- source.bat 
+</pre>
+
+#### Virtual environment
 
 This project is set up like a standard Python project.  The initialization
 process also creates a virtualenv within this project, stored under the .env
@@ -42,30 +86,61 @@ $ pip freeze > requirements.txt
 $ pip install -r requirements.txt
 ```
 
+#### CDK configuration file
+The `cdk.json` file tells the CDK Toolkit how to execute your app when you run `cdk deploy`.
+
+#### Setup project parameters in app.py
+The default project stack name is "my-fits-datalake", which will be used as the name of the CloudFormation template stack name and prefix for many of the resources created. You can change that to whatever you like in app.py
+
+```
+source_bucket_name = "fits-source-bucket"    #This is where your source FITS files are stored or will be stored.
+glue_database_name = "fits_datalake"        #This is the name of the database for the data catalog 
+
+MyFitsDatalakeStack(app, "my-fits-datalake", source_bucket_name=source_bucket_name, glue_database_name=glue_database_name)
+```
+
+There are two prameters you need to set for your own project. Please change them accordingly
+
+#### Synthesize and deploy the project
 At this point you can now synthesize the CloudFormation template for this code.
 
 ```
 $ cdk synth
+$ cdk deploy  my-fits-datalake -y
 ```
 
+#### Additional dependencies
 To add additional dependencies, for example other CDK libraries, just add
 them to your `setup.py` file and rerun the `pip install -r requirements.txt`
 command.
 
-## Useful commands
-
- * `cdk ls`          list all stacks in the app
- * `cdk synth`       emits the synthesized CloudFormation template
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk docs`        open CDK documentation
-
-Enjoy!
+#### After your deploy the project
+If you already have FITS files in your fits-source-bucket, run the following aws cli command
+```
+aws s3 cp --recursive --metadata {\"touched\":\"true\"} s3://<fits-source-bucket>/ s3://<fits-source-bucket>/
+```
+This will "touch" all the existing FITS files in the source bucket and trigger the header extraction lambda function to create header csv files in the <fits-destination-bucket>. 
 
 
 ## FITS Datalake Project
 
+### Architecture
+
 ![](images/science_datalake.png)
+
+Let's walk through what happens when a FITS file is uploaded to the source bucket
+
+- S3Put Event triggers the header extractor lambda function 
+- FITS header is extracted and saved into a csv file to the destination bucket
+- AWS Glue crawler update the database tables in AWS Glue data catalog
+
+
+The main resources involved in this reference architecture
+1. Amazon S3 bucket - existing source bucket (<fits-source-bucket>) - this is where FITS files are stored or will be added to. You can use folder structure inside the bucket. the folder name and file name of a FITS file will become part of the schema
+2. Amazon S3 bucket - created destination bucket (<fits-destination-bucket>) 
+3. AWS Glue crawer
+4. AWS Glue database and tables in AWS Glue data catalog
+
 
 ### Create a layer for the lambda function
 
@@ -87,9 +162,3 @@ use resources_layer/astropy.zip to create a new layer
         layer_numpy = lambda_.LayerVersion.from_layer_version_arn(self, "NumpyLayer", "arn:aws:lambda:us-east-1:668099181075:layer:AWSLambda-Python37-SciPy1x:22")
 ```
 Astropy depends on Numpy package, furtunately AWS has a public layer available (arn:aws:lambda:us-east-1:668099181075:layer:AWSLambda-Python37-SciPy1x:22)
-
-### Deploy the my-fits-datalake stack
-```
-$ cdk list # list stacks
-$ cdk deploy  my-fits-datalake -y
-```
--- my_fits_datalake/my_fits_datalake_stack.py
+++ my_fits_datalake/my_fits_datalake_stack.py
@@ -1,3 +1,4 @@
+
 from aws_cdk import (core, aws_s3 as s3, aws_lambda as lambda_,  aws_iam as iam_, aws_s3_notifications as s3_notifications) 
 from aws_cdk import custom_resources as custom_resources_
 from aws_cdk import aws_glue as glue_ 
@@ -7,19 +8,23 @@ import uuid
 
 
 class MyFitsDatalakeStack(core.Stack):
-
+    # Initial parameters
+    # source_bucket_name: name of the bucket where source FITS files are stored
+    # glue_database_name: name of the database in data catalog
     def __init__(self, scope: core.Construct, id: str, source_bucket_name: str, glue_database_name: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
 
-        # get the source bucket - this object is an IBucketProxy interface not a Buckt construct. Can not be used to add an event directly.
+        # get the source bucket - this object is an IBucketProxy interface, not a Buckt construct. 
+        # Can not be used to add an event directly. Instead, use a custom resource to add an event trigger later
         source_bucket = s3.Bucket.from_bucket_name(self, "MySourceBucket", bucket_name=source_bucket_name)
 
-        # create the new destination bucket
+        # create the new destination bucket - this bucket holds the csv file that containers the FITS header information
+        # the name of the bucket will be <stack-id>-fitsstorebucketXXXXXXXX-YYYYYYYYYYYYY
+        # e.g. my-fits-datalake-fitsstorebucket1234567f-098765432d
         target_bucket = s3.Bucket(self, "FITSSTORE_BUCKET")
 
-        #Add the astropy and numpy layers
+        # Add the astropy and numpy layers for the lambda function that is used as the event trigger on the source_bucket
         layer_astropy = lambda_.LayerVersion(self, 'AstroFitsioLayer', 
             code=lambda_.Code.from_asset("resources_layer/astropy.zip"),
             compatible_runtimes=[lambda_.Runtime.PYTHON_3_7]
@@ -27,7 +32,8 @@ class MyFitsDatalakeStack(core.Stack):
         # use an AWS provided layer for numpy
         layer_numpy = lambda_.LayerVersion.from_layer_version_arn(self, "NumpyLayer", "arn:aws:lambda:us-east-1:668099181075:layer:AWSLambda-Python37-SciPy1x:22")
 
-        # create the FITS header extractor
+        # create the FITS header extractor lambda function
+        # pass the FITSSTORE_BUCKET to the lambda function as an environment variable
         handler = lambda_.Function(self, "FITSHeaderExtractorHandler", 
                     runtime=lambda_.Runtime.PYTHON_3_7,
                     code=lambda_.Code.asset("resources"),
@@ -37,8 +43,10 @@ class MyFitsDatalakeStack(core.Stack):
                     ),
                     layers=[layer_astropy, layer_numpy]
                 )
+        
         # grant read access to handler on source bucket
         source_bucket.grant_read(handler)
+
         # Give the lambda resource based policy 
         # both source_arn and source_account is needed for security reason
         handler.add_permission('s3-trigger-lambda-s3-invoke-function',
@@ -47,16 +55,20 @@ class MyFitsDatalakeStack(core.Stack):
                               source_arn=source_bucket.bucket_arn,
                               source_account=self.account) 
 
-        # grant access to the handler    - this is a lot easier than adding policies, but not all constructs support this
+        # grant access to the handler    
+        # - this is a lot easier than adding policies, but not all constructs support this
         target_bucket.grant_read_write(handler)
 
 
         # map the put event to hanlder - this doesn't work as source_bucket is not really a Bucket object (IBucketProxy)
+        # You can use this approach if the bucket is created as a new Bucket object
         #notification = s3_notifications.LambdaDestination(handler)
         #source_bucket.add_object_created_notification(self, notification )
 
         # use custom resource to add an event trigger on the destnation bucket - 
-        # the custom resource creation makes an SDK call to create the event notification on the bucket
+        # the custom resource creation makes an SDK call to create the event notification on the 
+        # Action reference https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html
+        # Events reference https://docs.aws.amazon.com/AmazonS3/latest/dev/NotificationHowTo.html
         custom_s3_resource = custom_resources_.AwsCustomResource(self, 's3-putobject-custom-notification-resource',
             policy = custom_resources_.AwsCustomResourcePolicy.from_statements (
                 [
@@ -75,7 +87,7 @@ class MyFitsDatalakeStack(core.Stack):
                     "NotificationConfiguration": {
                         "LambdaFunctionConfigurations": [
                             {
-                                "Events": ['s3:ObjectCreated:*'],
+                                "Events": ['s3:ObjectCreated:*','s3:ObjectRemoved:*'],
                                 "LambdaFunctionArn": handler.function_arn,
                                 "Filter": {
                                     "Key": {
@@ -92,11 +104,13 @@ class MyFitsDatalakeStack(core.Stack):
                     f's3-notification-resource-{str(uuid.uuid1())}'),region=self.region
                 )
             )
-        #make sure the lambda function is created first 
+
+        # Make sure the lambda function is created first 
         custom_s3_resource.node.add_dependency(handler.permissions_node.find_child('s3-trigger-lambda-s3-invoke-function'))
        
        
         # create a glue crawler to build the data catalog
+        # Step 1 . create a role for AWS Glue
         glue_role = iam_.Role(self, "glue_role", 
             assumed_by=iam_.ServicePrincipal('glue.amazonaws.com'),
             managed_policies= [iam_.ManagedPolicy.from_managed_policy_arn(self, 'MyFitsCrawlerGlueRole', managed_policy_arn='arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole')]
@@ -104,16 +118,25 @@ class MyFitsDatalakeStack(core.Stack):
         # glue role needs "*" read/write - otherwise crawler will not be able to create tables (and no error messages in crawler logs)
         glue_role.add_to_policy(iam_.PolicyStatement( actions=['s3:GetObject', 's3:PutObject', 'lakeformation:GetDataAccess'], effect=iam_.Effect.ALLOW, resources=['*']))
         
-        # create a database in data catalog
+        # Step 2. create a database in data catalog
         db=glue_.Database(self, "MyFitsDatabase", database_name=glue_database_name)
-        crawler = glue_.CfnCrawler(self, "fits-datalake-crawler", 
+
+        # Step 3. create a crawler named "fitsdatalakecrawler-<hex>", and schedule to run hourly
+        # You can change the frequency based on your needs
+        # cron schedule format cron(Minutes Hours Day-of-month Month Day-of-week Year) 
+        glue_.CfnCrawler(self, "fits-datalake-crawler", 
             database_name= glue_database_name,
             role=glue_role.role_arn,
+            schedule={"ScheduleExpression":"cron(0 * * * ? *)"},
             targets={"s3Targets": [{"path":  target_bucket.bucket_name}]}, 
         )
+
+        # When your AWS Lake Formation Data catalog settings is not set to 
+        # "Use only IAM access control for new databases" or
+        # "Use only IAM access control for new tables in new databse"
+        # you need to grant additional permission to the data catalog database. 
         # in order for the crawler to run, we need to add some permissions to lakeformation
 
-        #lake_setting = lakeformation_.CfnDataLakeSettings(self, "MyFitsDatalakeSetting")
         location_resource = lakeformation_.CfnResource(self, "MyFitsDatalakeLocationResource", 
                 resource_arn= target_bucket.bucket_arn, 
                 use_service_linked_role=True
@@ -130,4 +153,3 @@ class MyFitsDatalakeStack(core.Stack):
             )
         #make sure the location resource is created first
         location_permission.node.add_dependency(location_resource)
-        
